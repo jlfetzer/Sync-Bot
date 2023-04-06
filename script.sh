edb0fde7c38da9f9f95d45b71b59bda150ac730f
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-06 11:04:52 +1000

    added bash script to invoke aws cli commands

---
+++ script.sh
@@ -0,0 +1,5 @@
+# get token - needed for IMDSv2
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
+
+# get instance ID
+curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id
