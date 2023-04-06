3b3ac24277aaff15f4d561faa4468f37290d4839
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-06 10:27:35 +0800

    maybe i should test code before committing. whoops.

--- script.sh
+++ script.sh
@@ -9,6 +9,6 @@ curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta
 webhook_url=$(aws secretsmanager get-secret-value --output text --query SecretString --secret-id slack_webhook_url --region us-east-1)
 curl -X POST -H 'Content-type: application/json' --data '{"text": "Buildkite did a thing"}' $webhook_url
 
-curl -L -O /tmp/master.zip https://github.com/fortra/impacket/archive/refs/heads/master.zip
+curl -L -o /tmp/master.zip https://github.com/fortra/impacket/archive/refs/heads/master.zip
 
 buildkite-agent artifact upload /tmp/master.zip
