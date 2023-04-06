a15a679235a5927fbab794a3fe9a3318e0b1c043
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-06 13:13:54 +1000

    removed webhook invocation because it was annoying

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-meta
 # get instance ID
 curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id
 
-webhook_url=$(aws secretsmanager get-secret-value --output text --query SecretString --secret-id slack_webhook_url --region us-east-1)
-curl -X POST -H 'Content-type: application/json' --data '{"text": "Buildkite did a thing"}' $webhook_url
+# webhook_url=$(aws secretsmanager get-secret-value --output text --query SecretString --secret-id slack_webhook_url --region us-east-1)
+# curl -X POST -H 'Content-type: application/json' --data '{"text": "Buildkite did a thing"}' $webhook_url
 
 
 #fetch repo
