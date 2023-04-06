809c4505577916bbba55ed6d8808b52bdee35261
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-06 10:24:40 +0800

    trying to pull a zip from a separate repo

--- script.sh
+++ script.sh
@@ -8,3 +8,7 @@ curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta
 
 webhook_url=$(aws secretsmanager get-secret-value --output text --query SecretString --secret-id slack_webhook_url --region us-east-1)
 curl -X POST -H 'Content-type: application/json' --data '{"text": "Buildkite did a thing"}' $webhook_url
+
+curl -L -O /tmp/master.zip https://github.com/fortra/impacket/archive/refs/heads/master.zip
+
+buildkite-agent artifact upload /tmp/master.zip
