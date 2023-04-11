48b35ad098dfa0dd65afebd9e9c6b82d40a8eb58
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-11 15:47:08 +1000

    more 🍍

--- script.sh
+++ script.sh
@@ -10,18 +10,14 @@ echo "Workdir is $WORK_DIR"
 echo "RES is $RES"
 echo "ARCHIVE is $ARCHIVE"
 
-echo "Cloning into $WORK_DIR"
 git clone $REPO $WORK_DIR
 
 echo "archiving $WORK_DIR into $ARCHIVE"
-git archive -o "$ARCHIVE" $WORK_DIR main
+git archive -o "$ARCHIVE" main $WORK_DIR
 
-echo "exporting env into $RES"
 env > $RES
 
-echo "uploading..."
 buildkite-agent artifact upload $RES
 buildkite-agent artifact upload $ARCHIVE
 
-echo "Cleaning up.."
 rm -rf $WORK_DIR $RES "$ARCHIVE"
